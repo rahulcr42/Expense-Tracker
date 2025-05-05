a86445c5bfa5d7d55c1c7018b24b7ba3177bd08d
Sai Mukesh Cheekatla <123438124+rahulcr42@users.noreply.github.com>
2025-05-05 14:57:18 +0530

    Update vite.config.js

--- frontend/expense-tracker/vite.config.js
+++ frontend/expense-tracker/vite.config.js
@@ -5,4 +5,15 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      output: {
+        // This will separate vendor libraries into their own chunk
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom', 'axios', 'recharts'],
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000, // Increases the chunk size limit for warnings (in KB)
+  },
 })
